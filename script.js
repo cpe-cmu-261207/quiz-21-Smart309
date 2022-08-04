bf5656fef74f15e1ec98410904eb378efe8356dc
Smart309 <pattharapon_takham@cmu.ac.th>
2022-08-04 14:11:44 +0700

    "UP R1"

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const geneRate = document.querySelector("#btn-random");
+const namE = document.querySelector("#p-name");
+const emaiL = document.querySelector("#p-email");
+const adD = document.querySelector("#p-address");
+const imgP = document.querySelector("#img-profile");
+const UC = document.querySelector("#div-user-card");
+callApi();
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+geneRate.onclick = () => {
+  UC.style.display = "none";
+};
