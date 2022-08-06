b3ec65f4387a7b513bdaab71016e0092b9747037
Smart309 <pattharapon_takham@cmu.ac.th>
2022-08-06 15:06:59 +0700

    "Add Data"

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ const adD = document.querySelector("#p-address");
 const imgP = document.querySelector("#img-profile");
 const UC = document.querySelector("#div-user-card");
 
-callApi();
-
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  imgP.src = resp.data.results[0].picture.large;
+  namE.innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  emaiL.innerText = resp.data.results[0].email;
+  adD.innerText =
+    resp.data.results[0].location.city +
+    " " +
+    resp.data.results[0].location.state +
+    " " +
+    resp.data.results[0].location.country;
+  console.log(resp.data.results[0]);
 }
 
 geneRate.onclick = () => {
-  UC.style.display = "none";
-  namE.innerText = resp.data.name;
+  callApi();
 };
